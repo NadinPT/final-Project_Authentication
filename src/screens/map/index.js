e22e3c1e8e9d9c613a6b248e2c7c3f901a6396ff
NadinePT <you@example.com>
2021-05-17 15:43:07 +0300

    add styles

--- src/screens/map/index.js
+++ src/screens/map/index.js
@@ -3,33 +3,33 @@ import "./style.scss";
 let map;
 
 window.initMap = () => {
-  navigator.geolocation.getCurrentPosition((pos) => {
-    //получаем наши координаты (нужно разрешить)
-    var crd = pos.coords;
     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: crd.latitude, lng: crd.longitude },
-      zoom: 8,
+        center: { lat: crd.latitude, lng: crd.longitude },
+        zoom: 8,
     });
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const infoWindow = new google.maps.InfoWindow();
+    navigator.geolocation.getCurrentPosition((pos) => {
+        //получаем наши координаты (нужно разрешить)
+        var crd = pos.coords;
+        const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const infoWindow = new google.maps.InfoWindow();
 
-    for (var i = 0; i < 15; i++) {
-      const marker = new google.maps.Marker({
-        position: {
-          lat: crd.latitude + Math.random(),
-          lng: crd.longitude + Math.random(),
-        },
-        map,
-        label: labels[i],
-        animation: google.maps.Animation.DROP,
+        for (var i = 0; i < 15; i++) {
+            const marker = new google.maps.Marker({
+                position: {
+                    lat: crd.latitude + Math.random(),
+                    lng: crd.longitude + Math.random(),
+                },
+                map,
+                label: labels[i],
+                animation: google.maps.Animation.DROP,
 
-        title: "Hello World!",
-      });
-      marker.addListener("click", () => {
-        infoWindow.close();
-        infoWindow.setContent(marker.getTitle());
-        infoWindow.open(marker.getMap(), marker);
-      });
-    }
-  });
+                title: "Hello World!",
+            });
+            marker.addListener("click", () => {
+                infoWindow.close();
+                infoWindow.setContent(marker.getTitle());
+                infoWindow.open(marker.getMap(), marker);
+            });
+        }
+    });
 };
